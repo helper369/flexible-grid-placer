f1ea8a25f39d4365bc8a1625a6c30d02ea75b3d6
helper369 <149539733+helper369@users.noreply.github.com>
2024-03-11 02:47:10 -0300

    Delete flexible-grid-placer directory

--- flexible-grid-placer/components/controler-sec/cell-size-lever/lever.js
+++
@@ -1,7 +0,0 @@
-//The value of the element might change in future
-
-let lever = document.querySelector("#lever");
-lever.addEventListener('input', ()=>{
-	flexGrid.cellsSize = lever.value;
-	flexGrid.update();
-});
